54df617bf94ac5b658dc5a8fee8f0860ded931f9
Ian Foehrweiser <ifoehrweiser2@huskers.unl.edu>
2022-08-25 19:53:44 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: NAME Ian FOehrweiser
+ *         EMAIL ifoehrweiser2@huskers.unl.edu
+ * Date: 8/23/2022
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Ian Foehrweiser!\n");
+  printf("My name is Ian Foehrweiser!!\n");
+    printf("My major is computer science!\n");
+  return 0;
+}
